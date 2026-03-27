b46fb5e6e171ddb8288b0f1f728e849b1a71e9a5
WindowCleanerPT <devotedfroglover@gmail.com>
2026-03-26 18:37:48 -0700

    Update main.mod.js

--- polyqol/1.0.0/main.mod.js
+++ polyqol/1.0.0/main.mod.js
@@ -1,34 +1,27 @@
 import { PolyMod } from "https://cdn.polymodloader.com/cb/PolyTrackMods/PolyModLoader/0.6.0/PolyTypes.js";
 
-export default class SoundboardMod extends PolyMod {
+class SoundboardMod extends PolyMod {
     init(pml) {
         this.pml = pml;
 
-        // get pmlapi (required for sound system)
         this.pApi = pml.getMod("pmlapi");
 
-        // create keybind category
         pml.registerBindCategory("Soundboard");
 
-        // KEY 1
         pml.registerKeybind("Beep", "sb_beep", "keydown", "KeyZ", null, () => {
             this.pApi.soundManager.playSound("beep", 1);
         });
 
-        // KEY 2
         pml.registerKeybind("Boop", "sb_boop", "keydown", "KeyX", null, () => {
             this.pApi.soundManager.playSound("boop", 1);
         });
 
-        // KEY 3
         pml.registerKeybind("Explosion", "sb_explode", "keydown", "KeyC", null, () => {
             this.pApi.soundManager.playSound("explode", 1);
         });
     }
 
     postInit() {
-        // register sounds AFTER init (IMPORTANT)
-
         this.pApi.soundManager.registerSound(
             "beep",
             ["https://actions.google.com/sounds/v1/cartoon/beep_short.ogg"]
@@ -45,3 +38,6 @@ export default class SoundboardMod extends PolyMod {
         );
     }
 }
+
+// ✅ THIS is the required fix
+export const polyMod = new SoundboardMod();
