186e148bb031644ed56cb983a5d908404d262e2b
WindowCleanerPT <devotedfroglover@gmail.com>
2026-03-27 16:41:20 -0700

    Update main.mod.js

--- polyqol/1.0.0/main.mod.js
+++ polyqol/1.0.0/main.mod.js
@@ -9,16 +9,16 @@ class SoundboardMod extends PolyMod {
         this.pml = pml;
         pml.registerBindCategory("Soundboard");
         pml.registerKeybind("Vine Boom", "sb_vineboom", "keydown", "Digit1", null, () => {
-            this.play("https://raw.githubusercontent.com/WindowCleanerPT/polyqol/main/sounds/vineboom.mp3");
+            this.play("https://cdn.jsdelivr.net/gh/WindowCleanerPT/polyqol/sounds/vineboom.mp3");
         });
         pml.registerKeybind("Car Honk", "sb_honk", "keydown", "Digit2", null, () => {
-            this.play("https://raw.githubusercontent.com/WindowCleanerPT/polyqol/main/sounds/honk.mp3");
+            this.play("https://cdn.jsdelivr.net/gh/WindowCleanerPT/polyqol/sounds/honk.mp3");
         });
         pml.registerKeybind("Beep", "sb_beep", "keydown", "Digit3", null, () => {
-            this.play("https://raw.githubusercontent.com/WindowCleanerPT/polyqol/main/sounds/beep.mp3");
+            this.play("https://cdn.jsdelivr.net/gh/WindowCleanerPT/polyqol/sounds/beep.mp3");
         });
         pml.registerKeybind("Motorcycle Rev", "sb_rev", "keydown", "Digit4", null, () => {
-            this.play("https://raw.githubusercontent.com/WindowCleanerPT/polyqol/main/sounds/rev.mp3");
+            this.play("https://cdn.jsdelivr.net/gh/WindowCleanerPT/polyqol/sounds/rev.mp3");
         });
     }
 }
