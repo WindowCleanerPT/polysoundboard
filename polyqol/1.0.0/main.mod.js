1f45acda5051a53d4a7979b8316bc10c2d1c81a3
WindowCleanerPT <devotedfroglover@gmail.com>
2026-03-27 16:27:52 -0700

    Update main.mod.js

--- polyqol/1.0.0/main.mod.js
+++ polyqol/1.0.0/main.mod.js
@@ -1,27 +1,10 @@
 import { PolyMod } from "https://cdn.polymodloader.com/cb/PolyTrackMods/PolyModLoader/0.6.0/PolyTypes.js";
 
 class SoundboardMod extends PolyMod {
-    play(url) {
-        const audio = new Audio(url);
-        audio.volume = 1;
-        audio.play();
-    }
-
     init = (pml) => {
         this.pml = pml;
         pml.registerBindCategory("Soundboard");
-        pml.registerKeybind("Beep", "sb_beep", "keydown", "KeyZ", null, () => {
-            console.log(this);
-            this.play("https://actions.google.com/sounds/v1/cartoon/beep_short.ogg");
-        });
-        pml.registerKeybind("Boop", "sb_boop", "keydown", "KeyX", null, () => {
-            this.play("https://actions.google.com/sounds/v1/cartoon/wood_plank_flicks.ogg");
-        });
-        pml.registerKeybind("Explosion", "sb_explode", "keydown", "KeyC", null, () => {
-            this.play("https://actions.google.com/sounds/v1/explosions/explosion.ogg");
-        });
     }
 }
 
-let polyMod = new SoundboardMod();
-export { polyMod };
+export let polyMod = new SoundboardMod();
