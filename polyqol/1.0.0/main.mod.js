4f9ad29f76b379747d3ffe4a8af34026f6665de0
WindowCleanerPT <devotedfroglover@gmail.com>
2026-03-26 18:45:11 -0700

    Update main.mod.js

--- polyqol/1.0.0/main.mod.js
+++ polyqol/1.0.0/main.mod.js
@@ -1,43 +1,30 @@
 import { PolyMod } from "https://cdn.polymodloader.com/cb/PolyTrackMods/PolyModLoader/0.6.0/PolyTypes.js";
 
 class SoundboardMod extends PolyMod {
+
+    play(url) {
+        const audio = new Audio(url);
+        audio.volume = 1;
+        audio.play();
+    }
+
     init(pml) {
         this.pml = pml;
-
-        this.pApi = pml.getMod("pmlapi");
 
         pml.registerBindCategory("Soundboard");
 
         pml.registerKeybind("Beep", "sb_beep", "keydown", "KeyZ", null, () => {
-            this.pApi.soundManager.playSound("beep", 1);
+            this.play("https://actions.google.com/sounds/v1/cartoon/beep_short.ogg");
         });
 
         pml.registerKeybind("Boop", "sb_boop", "keydown", "KeyX", null, () => {
-            this.pApi.soundManager.playSound("boop", 1);
+            this.play("https://actions.google.com/sounds/v1/cartoon/wood_plank_flicks.ogg");
         });
 
         pml.registerKeybind("Explosion", "sb_explode", "keydown", "KeyC", null, () => {
-            this.pApi.soundManager.playSound("explode", 1);
+            this.play("https://actions.google.com/sounds/v1/explosions/explosion.ogg");
         });
-    }
-
-    postInit() {
-        this.pApi.soundManager.registerSound(
-            "beep",
-            ["https://actions.google.com/sounds/v1/cartoon/beep_short.ogg"]
-        );
-
-        this.pApi.soundManager.registerSound(
-            "boop",
-            ["https://actions.google.com/sounds/v1/cartoon/wood_plank_flicks.ogg"]
-        );
-
-        this.pApi.soundManager.registerSound(
-            "explode",
-            ["https://actions.google.com/sounds/v1/explosions/explosion.ogg"]
-        );
     }
 }
 
-// ✅ THIS is the required fix
 export const polyMod = new SoundboardMod();
